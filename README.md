09f24e301da70b365fac8f571b32da6bb3df69fe
zhc <zhcmoe@163.com>
2018-07-12 10:27:16 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# 尝试写一个云盘系统
+````bash
+    install:npm i
+    run:npm run dev
+````
---
+++ index.js
@@ -0,0 +1,25 @@
+const Express = require('express');
+const app = Express();
+const multer = require('multer');
+const bodyParser = require('body-parser')
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+const upload = multer({
+    storage,
+    preservePath: true
+})
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+//app.use(upload()); // for parsing multipart/form-data
+app.post('/upload', upload.single('fieldname'), function (req, res, next) {
+    console.log(req)
+})
+app.listen(3000, function () {
+    console.log('servers run ::3000')
+})
